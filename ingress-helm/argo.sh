b7470e0bfb1de82cc548d66703ed5a2e6c1d5b19
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-28 11:00:47 +0530

    roboshop

--- ingress-helm/argo.sh
+++ ingress-helm/argo.sh
@@ -1,5 +1,5 @@
 argocd login $( kubectl get ingress -A | grep argocd | awk '{print $4}' | tail -1)  --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --skip-test-tls --grpc-web
 
-for app in frontend do;
+for app in frontend catalogue user cart shipping payment do;
 
 argocd app create ${app} --repo https://github.com/devps23/roboshop-helm.git --path chart --dest-server https://kubernetes.default.svc --dest-namespace default.svc --grpc-web --values values/${app}.yaml
