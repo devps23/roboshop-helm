a0ac74b422280ba6fdf55bb3295c7fbbe825a1da
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-29 13:44:58 +0530

    * to get loadbalancer service * to login argocd * to create app into argocd dashboard * sync app

--- ingress-helm/argo.sh
+++ ingress-helm/argo.sh
@@ -1,7 +1,29 @@
+loadbalancer = $(kubectl get ingress -A | grep argocd | awk '{print $4}' | tail -1)
+echo $loadbalancer
+
+while [ true ];
+do
+   nslookup  $loadbalancer
+   if [ $? eq 0 ]; then
+      echo "Load balancer not exists"
+      break;
+   fi
+   echo "Please wait until load balancer is active"
+done
+
+sleep10
+
+while [ true ];
+do
+  argocd admin initial-password -n argocd | head -1
+done
+
+sleep 2
+
 argocd login $( kubectl get ingress -A | grep argocd | awk '{print $4}' | tail -1)  --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --skip-test-tls --grpc-web
 
 for app in frontend catalogue user cart shipping payment; do
+argocd app create {{app}} --repo https://github.com/devps23/roboshop-helm.git --path chart --dest-server https://kubernetes.default.svc --dest-namespace default.svc --grpc-web --values values/{{app}}.yaml
 
-argocd app create ${app} --repo https://github.com/devps23/roboshop-helm.git --path chart --dest-server https://kubernetes.default.svc --dest-namespace default.svc --grpc-web --values values/${app}.yaml
-argocd app sync ${app}
+argocd app sync {{app}}
 done
